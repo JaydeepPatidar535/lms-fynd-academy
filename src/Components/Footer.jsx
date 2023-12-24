5dd55afb2ecad805e3d0b1c40a8aaa16a45e9939
JaydeepPatidar535 <122714310+JaydeepPatidar535@users.noreply.github.com>
2023-12-25 01:42:48 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
                     Copyright {year} | All rights reserved Learnovation
                 </section>
                 <section className='flex items-center justify-center gap-5 text-2xl text-white'>
-                    <a className='hover:text-yellow-500 transition-all ease-in-out duration-300' href='https://github.com/JaydeepPatidar535/LMS---Lernovation' target='_blank'>
+                    <a className='hover:text-yellow-500 transition-all ease-in-out duration-300' href='https://github.com/JaydeepPatidar535/Learnovation' target='_blank'>
                         <BsGithub />
                     </a>
                     <a className='hover:text-yellow-500 transition-all ease-in-out duration-300' href='https://www.linkedin.com/in/jaydeep-patidar-810a1b253' target='_blank'>
